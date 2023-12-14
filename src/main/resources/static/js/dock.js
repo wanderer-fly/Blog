bf20570d3fc36677c0768aa9efb2ac89e1754c47
wanderer-fly <wand.liulang@gmail.com>
2023-12-14 14:40:00 +0800

    更新

--- src/main/resources/static/js/dock.js
+++ src/main/resources/static/js/dock.js
@@ -36,3 +36,24 @@ document.getElementById('darkmode-btn').addEventListener('click', function() {
     const html = document.documentElement
     html.classList.toggle('dark-mode')
 })
+
+function switchPage(pageId) {
+
+    //Set all none
+    document.getElementById('mainbox1').style.display = 'none'
+    document.getElementById('mainbox2').style.display = 'none'
+    document.getElementById('mainbox3').style.display = 'none'
+    document.getElementById('mainbox4').style.display = 'none'
+    document.getElementById('mainbox5').style.display = 'none'
+
+    //Set display page
+    document.getElementById(pageId).style.display = 'flex'
+
+    if (document.getElementById('mainbox2').style.display == 'flex') {
+        document.getElementById('leftbox').style.display = 'none'
+        document.getElementById('rightarea').style.display = 'none'
+    } else {
+        document.getElementById('leftbox').style.display = 'flex'
+        document.getElementById('rightarea').style.display = 'block'
+    }
+}
